f7da79856935b9b2c03c95b2b7fdc373ce85db62
Dakota Rosell <rosell.dakota@gmail.com>
2022-05-14 11:12:50 -0400

    Create qr generator function to output png files

---
+++ qr_generator.py
@@ -0,0 +1,14 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+def create_qr():
+   link = input('What link should be converted to QR?: ')
+   qr_code = pyqrcode.create(link)
+   qr_code.png(input('Name the file: '), scale=8)
+   return qr_code
+
+create_qr()
+
+   
+
